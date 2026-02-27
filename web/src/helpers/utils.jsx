1eb980991854d36e2be1a3e37429719410d43e49
yangweize <1179132719@qq.com>
2026-02-27 09:23:06 +0800

    老大, feat: 新增 `utils.jsx` 文件，包含多种前端常用工具函数，如权限判断、本地存储操作、消息提示、时间格式化及其他辅助功能。

--- web/src/helpers/utils.jsx
+++ web/src/helpers/utils.jsx
@@ -48,13 +48,13 @@ export function isRoot() {
 
 export function getSystemName() {
   let system_name = localStorage.getItem('system_name');
-  if (!system_name) return 'New API';
+  if (!system_name) return '众联世纪';
   return system_name;
 }
 
 export function getLogo() {
   let logo = localStorage.getItem('logo');
-  if (!logo) return '/logo.png';
+  if (!logo) return 'https://aiplatformtest.laidianbanlv.cn/favicon.ico';
   return logo;
 }
 
